5f8f89c3cedf83d1894070eeb8915a7c88c9c3ba
Victor Erike <verike7@gmail.com>
2023-03-11 01:45:05 +0100

    Initial commit

---
+++ src/config/constants.config.js
@@ -0,0 +1,5 @@
+
+
+module.exports.maxAgeForCookies = 1000 * 60 * 60 * 24 * 3 /* For 1 day */ /* (Calculated in miliseconds) */
+
+module.exports.maxAgeForToken = 60 * 60 * 24 * 3 /* For a day */ /* (Calculated in seconds) */
